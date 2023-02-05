3ccdfe3b57726f0d423684b847f2bb3d261f562d
Janai06 <91900770+Janai06@users.noreply.github.com>
2023-02-05 06:12:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
+const welcome = document.querySelector('.welcome');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
-
-
+ 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
+    welcome.classList.add('active');
 });
 
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
+    welcome.classList.add('active');
 });
 
 btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
+    welcome.classList.add('active');
 });
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+    welcome.classList.remove('active');
 });
+
+document.querySelector('#welcomeLogin').addEventListener('click', e =>
+  document.querySelector('#btnLogin').click());
